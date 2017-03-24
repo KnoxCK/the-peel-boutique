9dcbfe0cee7473b74673231fae6096441e4a5486
Clarissa Knox <knox.ck@gmail.com>
2017-03-24 18:12:26 +0000

    adding to heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :treatments, :contact, :faqs, :shop ]
 
   def home
   end
+
+  def about
+  end
+
+  def treatments
+  end
+
+  def contact
+  end
+
+  def faqs
+  end
+
+  def shop
+  end
+
 end
