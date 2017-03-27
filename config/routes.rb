f2c002c95a45815adc878906faceec5619b417c7
Clarissa Knox <knox.ck@gmail.com>
2017-03-27 10:40:45 +0100

    booking page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   get 'shop', to: 'pages#shop'
   get 'faqs', to: 'pages#faqs'
+  get 'booking', to: 'pages#booking'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
