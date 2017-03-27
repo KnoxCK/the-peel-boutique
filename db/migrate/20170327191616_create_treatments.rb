f5101dddc5a68b3785fe9b08f55e77690e61726f
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-03-27 20:19:21 +0100

    treatment model added and database created

---
+++ db/migrate/20170327191616_create_treatments.rb
@@ -0,0 +1,10 @@
+class CreateTreatments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :treatments do |t|
+      t.string :sku
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
