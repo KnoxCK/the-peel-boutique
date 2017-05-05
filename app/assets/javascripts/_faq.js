87939c95c81fd4b92277b4eef77cd4e3d68060bb
Clarissa Knox <knox.ck@gmail.com>
2017-05-05 01:53:10 +0100

    faqs page done

---
+++ app/assets/javascripts/_faq.js
@@ -0,0 +1,49 @@
+// Testimonial section
+$(document).on('click', '.arrow-1', function(e) {
+  	$(".answer-1").removeClass("hidden");
+  	$(".arrow-1").addClass("hidden");
+  	$(".arrow-1a").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.arrow-2', function(e) {
+  	$(".answer-2").removeClass("hidden");
+  	$(".arrow-2").addClass("hidden");
+  	$(".arrow-2a").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.arrow-3', function(e) {
+  	$(".answer-3").removeClass("hidden");
+  	$(".arrow-3").addClass("hidden");
+  	$(".arrow-3a").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.arrow-4', function(e) {
+  	$(".answer-4").removeClass("hidden");
+  	$(".arrow-4").addClass("hidden");
+  	$(".arrow-4a").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.arrow-5', function(e) {
+  	$(".answer-5").removeClass("hidden");
+  	$(".arrow-5").addClass("hidden");
+  	$(".arrow-5a").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.arrow-6', function(e) {
+  	$(".answer-6").removeClass("hidden");
+  	$(".arrow-6").addClass("hidden");
+  	$(".arrow-6a").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.arrow-7', function(e) {
+  	$(".answer-7").removeClass("hidden");
+  	$(".arrow-7").addClass("hidden");
+  	$(".arrow-7a").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.arrow-8', function(e) {
+  	$(".answer-8").removeClass("hidden");
+  	$(".arrow-8").addClass("hidden");
+  	$(".arrow-8a").removeClass("hidden");
+	e.preventDefault();
+});
