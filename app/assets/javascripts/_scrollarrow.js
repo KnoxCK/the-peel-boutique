fb79f25f999661be2675d0648fa0d97f5901be1b
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-07-20 19:09:37 +0100

    landing scroll down fix

--- app/assets/javascripts/_scrollarrow.js
+++ app/assets/javascripts/_scrollarrow.js
@@ -1,7 +1,16 @@
-$('a[href^="#"]').click(function(){
-	var arrow = $(this).attr("href");
-	    $('html, body').animate({
-	        scrollTop:$(enter).offset().top
-	    }, 'medium');
+// $('a[href^="#"]').click(function(){
+// 	var arrow = $(this).attr("href");
+// 	    $('html, body').animate({
+// 	        scrollTop:$(enter).offset().top
+// 	    }, 'medium');
 
-return false;});
+// return false;});
+
+$(document).ready(function(){
+  $('.enter-btn').click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+          scrollTop:$('.landing-page-content').offset().top
+      }, 1000);
+  })
+})
