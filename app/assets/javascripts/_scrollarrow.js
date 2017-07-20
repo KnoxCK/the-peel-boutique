f96022f944553f86e2ae3fd74908eae21a406f9d
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-07-20 19:10:41 +0100

    hide landing strips

--- app/assets/javascripts/_scrollarrow.js
+++ app/assets/javascripts/_scrollarrow.js
@@ -1,11 +1,3 @@
-// $('a[href^="#"]').click(function(){
-// 	var arrow = $(this).attr("href");
-// 	    $('html, body').animate({
-// 	        scrollTop:$(enter).offset().top
-// 	    }, 'medium');
-
-// return false;});
-
 $(document).ready(function(){
   $('.enter-btn').click(function(e){
     e.preventDefault();
