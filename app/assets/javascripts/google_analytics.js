db8b95c57c6b453a080049eaac1eb32fc60f14a1
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-03 14:59:10 +0100

    coffee

--- app/assets/javascripts/google_analytics.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('turbolinks:load', function(event) {
-  if (typeof ga === 'function') {
-    ga('set', 'location', event.data.url);
-    ga('send', 'pageview');
-  }
-});
