89dc2fbab750473ae05083bb2d87b0dd5edc289f
Clarissa Knox <knox.ck@gmail.com>
2018-09-04 12:23:19 +0100

    heroku

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,14 @@
-class OrdersController < ApplicationController
-  skip_before_action :authenticate_user!
+# class OrdersController < ApplicationController
+#   skip_before_action :authenticate_user!
 
-  def show
-    @order = Order.where(state: 'paid').find(params[:id])
-  end
+#   def show
+#     @order = Order.where(state: 'paid').find(params[:id])
+#   end
 
-  def create
-    @treatment = Treatment.find(params[:treatment_id])
-    order = Order.create!(sku: @treatment.sku, amount: @treatment.price, state: 'pending')
+#   def create
+#     @treatment = Treatment.find(params[:treatment_id])
+#     order = Order.create!(sku: @treatment.sku, amount: @treatment.price, state: 'pending')
 
-    redirect_to new_order_payment_path(order)
-  end
-end
+#     redirect_to new_order_payment_path(order)
+#   end
+# end
