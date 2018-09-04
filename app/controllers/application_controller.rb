71db5a8d621d3ce90960adabfd6bb2fb7d160129
Clarissa Knox <knox.ck@gmail.com>
2018-09-04 12:52:53 +0100

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :set_promotion
+  before_action :authenticate_user!
+
+  # , :set_promotion
 
   private
 
-  def set_promotion
-    @promotion = Treatment.first
-  end
+  # def set_promotion
+  #   @promotion = Treatment.first
+  # end
 end
