96ac20c6359fda05620f268e903e1a8f12eae7d3
Clarissa Knox <knox.ck@gmail.com>
2019-05-29 13:24:43 +0100

    holding page changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'treatments', to: 'pages#treatments'
-  get 'contact', to: 'pages#contact'
-  get 'shop', to: 'pages#shop'
-  get 'faqs', to: 'pages#faqs'
-  get 'booking', to: 'pages#booking'
+  get 'about', to: 'pages#home' #change back to pages#about
+  get 'treatments', to: 'pages#home' #change back to pages#treatments
+  get 'contact', to: 'pages#home' #change back to pages#contact
+  get 'shop', to: 'pages#home' #change back to pages#shop
+  get 'faqs', to: 'pages#home' #change back to pages#faqs
+  get 'booking', to: 'pages#home' #change back to pages#booking
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
